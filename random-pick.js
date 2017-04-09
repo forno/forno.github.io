7c21cc398f179ea470272ec94079b7bccbddb087
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-04-10 03:47:52 +0900

    Add alerts for invalid input

--- random-pick.js
+++ random-pick.js
@@ -4,23 +4,34 @@ function getRandomInt(min, max) {
 
 $('#resultModal').on('show.bs.modal', function(e) {
   "use strict";
+  var alerts = []
   var form = $(e.relatedTarget).parents("form")
   const targets = form.find("textarea").val().split('\n')
-  const count = parseInt(form.find("input").val(), 10)
+  var count = parseInt(form.find("input").val(), 10)
 
-  for (var i = 0; i < count; ++i)
-    targets[i] = targets[getRandomInt(i, targets.length)]
+  if (targets.length <= count) {
+    count = targets.length
+    alerts.push(
+      `<div class="alert alert-warning" role="alert">
+        <strong>Warning!</strong> Too small number. I pick every members.
+      </div>`)
+  }
+
+  for (let i = 0; i < count; ++i) {
+    let rand = getRandomInt(i, targets.length);
+    [targets[i], targets[rand]] = [targets[rand], targets[i]] 
+  }
   const chosen = targets.slice(0, count)
                         .filter(function(v){return v.length != 0})
                         .map(function(v){return `<li>${v}</li>`})
-  const send = chosen.length !== 0 ?
-               chosen.join("") :
-               `<div class="alert alert-warning" role="alert">
-                 <strong>Warning!</strong> Better check yourself, you're not looking too good.
-               </div>`
+  const send = chosen.length !== 0 ? "<ul>" + chosen.join("") + "</ul>" : "";
+  if (send === "")
+    alerts.push(
+      `<div class="alert alert-danger" role="alert">
+        <strong>Oh snap!</strong> Change a few things up and try submitting again.
+      </div>`)
 
-  var modal = $(this)
-  modal.find(".modal-body").html(send)
-  if (chosen.length != 0)
-    modal.find(".modal-body").wrapInner("<ul />");
+  var modalBody = $(this).find(".modal-body")
+  modalBody.html(send)
+  modalBody.append(alerts.join(""))
 })
