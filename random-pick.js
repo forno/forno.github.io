b92e2ad142c46f070f8a9896b53f19bf1025dee0
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-04-10 03:10:31 +0900

    Add js for random-pick

--- random-pick.js
+++ random-pick.js
@@ -1,3 +1,26 @@
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
 $('#resultModal').on('show.bs.modal', function(e) {
   "use strict";
+  var form = $(e.relatedTarget).parents("form")
+  const targets = form.find("textarea").val().split('\n')
+  const count = parseInt(form.find("input").val(), 10)
+
+  for (var i = 0; i < count; ++i)
+    targets[i] = targets[getRandomInt(i, targets.length)]
+  const chosen = targets.slice(0, count)
+                        .filter(function(v){return v.length != 0})
+                        .map(function(v){return `<li>${v}</li>`})
+  const send = chosen.length !== 0 ?
+               chosen.join("") :
+               `<div class="alert alert-warning" role="alert">
+                 <strong>Warning!</strong> Better check yourself, you're not looking too good.
+               </div>`
+
+  var modal = $(this)
+  modal.find(".modal-body").html(send)
+  if (chosen.length != 0)
+    modal.find(".modal-body").wrapInner("<ul />");
 })
