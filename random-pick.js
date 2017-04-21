fe0b50eafd9cb163b7ec2ee8496bf08ad6cc8ad0
Doi Yusuke <o111027y@mail.kyutech.jp>
2017-04-21 15:15:33 +0900

    Refactor modal update

--- random-pick.js
+++ random-pick.js
@@ -46,15 +46,16 @@ $(function(){
     modalBody.find(".error").html(alerts.join(""))
 
     var sender = modalBody.find(".result").empty()
-    var noticer = modalBody.find(".notice").html('<span class="text-muted">Now picking...</span>')
-    if (chosen.length)
+    if (!chosen.length)
+      modalBody.find(".notice").html('<span class="text-danger">Chosen members are nothing.</span>')
+    else {
+      var noticer = modalBody.find(".notice").html('<span class="text-muted">Now picking...</span>')
       intervalID = setInterval(function(){
         sender.append(chosen.pop())
         if (!chosen.length) {
           clearInterval(intervalID)
           noticer.html('<span class="text-success">Completed</span>')
         }}, 1000)
-    else
-      noticer.html('<span class="text-danger">Chosen members are nothing.</span>')
+    }
   })
 })
