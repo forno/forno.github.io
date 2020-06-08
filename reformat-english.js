f28c84557c60358cf28f99cdd4e4bfbbc424a0d7
FORNO <forno@xmaho.link>
2020-06-08 18:49:54 +0900

    Fix reformat english program

--- reformat-english.js
+++ reformat-english.js
@@ -3,12 +3,12 @@ $(function(){
 
  $('#resultModal').on('show.bs.modal', function(e) {
   var text = $('#english-text').val()
-  var text_body = $(this).find(".modal-body p")
+  var text_body = $(this).find('.modal-body p')
 
   if (!text) {
-    text_body.text("Error: invalid values")
+    text_body.text('Error: invalid values')
   } else {
-    text_body.text(text.replace('\n', ' ').replace('.', ".\n"))
+    text_body.html($('<span />').text(text.replace(/\n/g, ' ')).html().replace(/\./g, '.<br />'))
   }
   })
 })
