e0db4b6fcdd2c3a1d4e87789440e75ad0b6a8816
FORNO <forno@xmaho.link>
2020-06-09 02:51:32 +0900

    Add section and copy button

--- reformat-english.js
+++ reformat-english.js
@@ -1,14 +1,24 @@
 $(function(){
   "use strict";
 
- $('#resultModal').on('show.bs.modal', function(e) {
-  var text = $('#english-text').val()
-  var text_body = $(this).find('.modal-body p')
+  var modal = $('#resultModal')
+  modal.on('show.bs.modal', function(e) {
+    var text = $('#english-text').val()
+    var text_body = $(this).find('.modal-body p')
 
-  if (!text) {
-    text_body.text('Error: invalid values')
-  } else {
-    text_body.html($('<span />').text(text.replace(/\n/g, ' ')).html().replace(/\./g, '.<br />'))
-  }
+    if (!text) {
+      text_body.text('Error: invalid values')
+    } else {
+      text_body.html($('<span />').text(text.replace(/\n/g, ' ')).html().replace(/\.   */g, '. <br />').replace(/\. ?/g, '.<br />'))
+    }
+  })
+
+  modal.find('.modal-footer .btn-primary').on('click', function() {
+    var textarea = $('<textarea></textarea>')
+    textarea.text($('#english-text').val().replace(/\n/g, ' ').replace(/\.   */g, '. \n').replace(/\. ?/g, '.\n'))
+    $(this).append(textarea)
+    textarea.select()
+    document.execCommand('copy')
+    textarea.remove()
   })
 })
