22c8e3b7f2ae0accc3cdd0966d26c6fd76277b8b
FORNO <forno@xmaho.link>
2020-06-08 18:22:29 +0900

    Add reformat-english.js

---
+++ reformat-english.js
@@ -0,0 +1,14 @@
+$(function(){
+  "use strict";
+
+ $('#resultModal').on('show.bs.modal', function(e) {
+  var text = $('#english-text').val()
+  var text_body = $(this).find(".modal-body p")
+
+  if (!host) {
+    text_body.text("Error: invalid values")
+  } else {
+    text_body.text(text.replace('\n', ' ').replace('.', ".\n"))
+  }
+  })
+})
