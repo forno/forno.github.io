af595a9b73cc0f936d6f8d29d61b7777bcac808c
FORNO <forno@xmaho.link>
2020-06-10 15:53:32 +0900

    Rename Link to link

--- src/pages/material-ui.js
+++ src/pages/material-ui.js
@@ -3,7 +3,7 @@ import Container from "@material-ui/core/Container";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
 import ProTip from "../components/ProTip";
-import Link from "../components/Link";
+import Link from "../components/link";
 import Copyright from "../components/copyright";
 
 export default function Index() {
