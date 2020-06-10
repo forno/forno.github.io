2089a06e6cebe6f70c649323197a011e5d4f7667
FORNO <forno@xmaho.link>
2020-06-10 17:05:57 +0900

    Change theme to dark

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,45 +8,20 @@ import SEO from "./seo";
 import Header from "./header";
 import Copyright from "./copyright";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    minHeight: '100vh',
-  },
-  main: {
-    marginTop: theme.spacing(8),
-    marginBottom: theme.spacing(2),
-  },
-  footer: {
-    padding: theme.spacing(3, 2),
-    marginTop: 'auto',
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
-  },
-}));
-
-const Layout = ({ children, pageTitle, createdYear }) => {
-  const classes = useStyles();
-
-  return (
-    <>
-      <SEO title={pageTitle} />
-      <div className={classes.root}>
-        <CssBaseline />
-        <Header />
-        <Container component="main" className={classes.main} maxWidth="sm">
-          {children}
-        </Container>
-        <footer className={classes.footer}>
-          <Container maxWidth="sm">
-            <Copyright createdYear={createdYear} />
-          </Container>
-        </footer>
-      </div>
-    </>
-  );
-};
+const Layout = ({ children, pageTitle, createdYear }) => (
+  <>
+    <SEO title={pageTitle} />
+    <Header />
+    <Container component="main" maxWidth="sm">
+      {children}
+    </Container>
+    <footer>
+      <Container maxWidth="sm">
+        <Copyright createdYear={createdYear} />
+      </Container>
+    </footer>
+  </>
+);
 
 Layout.defaultProps = {
   createdYear: new Date().getFullYear(),
