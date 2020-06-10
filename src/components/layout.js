e263a7a4539d092248e46f2f61372af41178c334
FORNO <forno@xmaho.link>
2020-06-10 16:14:13 +0900

    Update layout with material-ui

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,33 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { useStaticQuery, graphql } from "gatsby";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+import Link from "@material-ui/core/Link";
 
 import SEO from "./seo";
 import Header from "./header";
 import Copyright from "./copyright";
-import "./layout.css";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  main: {
+    marginTop: theme.spacing(8),
+    marginBottom: theme.spacing(2),
+  },
+  footer: {
+    padding: theme.spacing(3, 2),
+    marginTop: 'auto',
+    backgroundColor:
+      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  },
+}));
 
 const Layout = ({ children, pageTitle, createdYear }) => {
   const data = useStaticQuery(graphql`
@@ -13,28 +35,27 @@ const Layout = ({ children, pageTitle, createdYear }) => {
       site {
         siteMetadata {
           title
-          author
         }
       }
     }
   `);
+	const classes = useStyles();
 
   return (
     <>
       <SEO title={pageTitle} />
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
-        <main>{children}</main>
-        <footer>
-          <Copyright createdYear={createdYear} author={data.site.siteMetadata.author} />
-        </footer>
-      </div>
+			<div className={classes.root}>
+				<CssBaseline />
+      	<Header siteTitle={data.site.siteMetadata.title} />
+				<Container component="main" className={classes.main} maxWidth="sm">
+					{children}
+				</Container>
+				<footer className={classes.footer}>
+					<Container maxWidth="sm">
+          	<Copyright createdYear={createdYear} />
+					</Container>
+				</footer>
+			</div>
     </>
   );
 };
