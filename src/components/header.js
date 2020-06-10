6272375b6adbc8894d56470ec372c9ed1ecdaa6e
FORNO <forno@xmaho.link>
2020-06-10 19:35:43 +0900

    Add drawer to top menu

--- src/components/header.js
+++ src/components/header.js
@@ -2,11 +2,16 @@ import { useStaticQuery, graphql } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
 import AppBar from "@material-ui/core/AppBar";
-import Container from "@material-ui/core/Container";
-import Typography from "@material-ui/core/Typography";
-import Toolbar from "@material-ui/core/Toolbar";
+import Drawer from "@material-ui/core/Drawer";
 import IconButton from "@material-ui/core/IconButton";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 import MenuIcon from "@material-ui/icons/Menu";
+import CodeIcon from '@material-ui/icons/Code';
 import Link from "./link";
 
 const Header = ({ siteTitle }) => {
@@ -22,21 +27,44 @@ const Header = ({ siteTitle }) => {
 
   const headerTitle = siteTitle || data.site.siteMetadata.title;
 
+  const [state, setState] = React.useState({ left: false });
+  const toggleDrawer = (anchor, open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [anchor]: open });
+  }
+
   return (
-    <AppBar position="static">
-      <Container maxWidth="sm">
-      <Toolbar>
-        <IconButton edge="start" color="inherit" aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <Link to="/" color="inherit">
-          <Typography component="h1" variant="h6">
-            {headerTitle}
-          </Typography>
-        </Link>
-      </Toolbar>
-      </Container>
-    </AppBar>
+    <>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton onClick={toggleDrawer("left", true)} edge="start" color="inherit" aria-label="left">
+            <MenuIcon />
+          </IconButton>
+          <Link to="/" color="inherit">
+            <Typography component="h1" variant="h6">
+              {headerTitle}
+            </Typography>
+          </Link>
+        </Toolbar>
+      </AppBar>
+      <Drawer anchor="left" open={state["left"]} onClose={toggleDrawer("left", false)}>
+        <div
+          role="presentation"
+          onClick={toggleDrawer("left", false)}
+          onKeyDown={toggleDrawer("left", false)}
+        >
+          <List>
+            <ListItem>
+              <ListItemIcon><CodeIcon /></ListItemIcon>
+              <ListItemText primary="Here is menu." />
+            </ListItem>
+          </List>
+        </div>
+      </Drawer>
+    </>
   )
 };
 
