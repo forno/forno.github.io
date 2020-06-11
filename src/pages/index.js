8329e3756dd28c60440deaceb87b17cc3a2d18c8
FORNO <forno@xmaho.link>
2020-06-11 17:15:28 +0900

    Update index contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,9 @@ import React from "react";
 import Layout from "../components/layout";
 
 const IndexPage = () => (
-  <Layout pageTitle="Home" createdYear="2020">
-    <h2>Hi people</h2>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+  <Layout pageTitle="Home" createdYear="2017">
+    <h2>FORNO's portfolio</h2>
+    <p>Now avalable are Tools only.</p>
   </Layout>
 );
 
