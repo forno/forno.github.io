f073335046f6463fb6e76d334f7a5c730dbd58d5
FORNO <forno@xmaho.link>
2020-06-11 17:19:20 +0900

    Remove unused import and refactor regexp

--- src/pages/tools/reformat-english.js
+++ src/pages/tools/reformat-english.js
@@ -1,10 +1,5 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogActions from "@material-ui/core/DialogActions";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogTitle from "@material-ui/core/DialogTitle";
 import Popover from "@material-ui/core/Popover";
 import TextField from "@material-ui/core/TextField";
 import AssignmentIcon from '@material-ui/icons/Assignment';
@@ -17,30 +12,16 @@ const ReformatEnglish = () => {
   const handleChange = (event) => {
     const newText = event.target.value
               .replace(/\n/g, " ")
-              .replace(/\.   */g, ". \n")
+              .replace(/\. {2,}/g, ". \n")
               .replace(/\. ?/g, ".\n");
     setText(newText);
   };
-
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
 
   const [anchorEl, setAnchorEl] = React.useState(null);
   const handleAnchorOpen = (event) => setAnchorEl(event.currentTarget);
   const handleAnchorClose = () => setAnchorEl(null);
   const anchorOpen = Boolean(anchorEl);
   const anchorId = anchorOpen ? 'copied-popover' : undefined;
-
-  const descriptionElementRef = React.useRef(null);
-  React.useEffect(() => {
-    if (open) {
-      const { current: descriptionElement } = descriptionElementRef;
-      if (descriptionElement !== null) {
-        descriptionElement.focus();
-      }
-    }
-  }, [open]);
 
   return (
     <>
