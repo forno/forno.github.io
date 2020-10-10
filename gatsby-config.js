8f227b00cb284ad6584213ec7c48f12cfc4736bc
FORNO <forno@xmaho.link>
2020-10-10 20:37:40 +0900

    Refactor image

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,7 +5,6 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
         path: path.join(__dirname, `..`, `src`, `images`),
       },
     },
