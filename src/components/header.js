250bdcda7d4ed45861fddced74d1f903999d4656
FORNO <forno@xmaho.link>
2020-10-11 21:28:47 +0900

    Add dark and white toggle switch to header

--- src/components/header.js
+++ src/components/header.js
@@ -1,36 +1,48 @@
 /** @jsx jsx */
-import { jsx } from "theme-ui"
+import { Button, Flex, jsx } from "theme-ui"
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
+import { useColorMode } from "theme-ui"
 
-const Header = ({ siteTitle }) => (
-  <header
-    sx={{
-      bg: `darken`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
+const Header = ({ siteTitle }) => {
+  const [colorMode, setColorMode] = useColorMode();
+
+  return (
+    <header
       sx={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        bg: `darken`,
+        marginBottom: `1.45rem`,
       }}
     >
-      <h1 sx={{ margin: 0 }}>
-        <Link
-          to="/"
-          sx={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
+      <Flex
+        sx={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `1.45rem 1.0875rem`,
+        }}
+      >
+        <h1 sx={{ margin: 0, flex: `1 1 auto` }}>
+          <Link
+            to="/"
+            sx={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <Button
+          onClick={() =>
+            setColorMode(colorMode === "default" ? "dark" : "default")
+          }
         >
-          {siteTitle}
-        </Link>
-      </h1>
-    </div>
-  </header>
-)
+          Toggle {colorMode === "default" ? "dark" : "Light"}
+        </Button>
+      </Flex>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
