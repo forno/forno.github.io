00d52233a387571369c2f17566ccb988054d0649
FORNO <forno@xmaho.link>
2020-10-13 08:42:39 +0900

    Add avatar image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import { Link } from 'gatsby-theme-material-ui'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
-import { Typography } from '@material-ui/core'
+import { graphql } from "gatsby";
+import Img from "gatsby-image";
 
-export default () => (
+
+export default ({ data }) => (
   <Layout createdAt={2017}>
     <SEO title="Home" />
     <h1>フォルノの情報サイト</h1>
     <p>こんにちは「フォルノ」です．通称「ふぉるにゃー」です．</p>
+    <Img fluid={data.file.childImageSharp.fluid} alt="FORNO's avatar" sx={{maxWidth : 250}}/>
     <h2>Just on the site works</h2>
     <p>Toolsにはこのサイトで利用できる便利機能があります．</p>
     <ul>
@@ -16,3 +20,15 @@ export default () => (
     </ul>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "icon.png"}) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
